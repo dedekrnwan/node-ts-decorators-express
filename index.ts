fa450d1a957c93d1d40031e6d8370a9cc82ef8f6
DEDE KURNIAWAN <PJT184450@PTI.LOCAL>
2019-06-24 19:36:58 +0700

    240619 - update

--- index.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * express
- * Copyright(c) 2019-2024 Dede kurniawan
- * MIT Licensed
- */
-import "reflect-metadata"
-
-export * from "./src"
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/*!
+ * express
+ * Copyright(c) 2019-2024 Dede kurniawan
+ * MIT Licensed
+ */
+import "reflect-metadata"
+
 export * from "./decorators"
 export * from "./attach"
 export * from "./interfaces"
