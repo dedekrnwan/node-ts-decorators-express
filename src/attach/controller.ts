7b89c0a5f505f4cb5c9fccfb85d1e747b798b775
DEDE KURNIAWAN <PJT184450@PTI.LOCAL>
2019-06-25 15:58:49 +0700

    250619 - adding ClassMiddlewares

--- src/attach/controller.ts
+++ src/attach/controller.ts
@@ -14,11 +14,14 @@ export default (app:express.Application, controllers:Array<any> , uses:string=''
         const routes:Array<IRoutes> = Reflect.getMetadata(metakey.routes, controller);
         const router:any = express.Router(); 
         routes.forEach((route:any) => {
+            //metadata class middleware
+            let middlewares_class:IMiddlewares = Reflect.getMetadata(metakey.middlewares, controller);
+            //metadata method middleware
             let middlewares:IMiddlewares = Reflect.getMetadata(metakey.middlewares, controller, route.function);
+            //metadata route of method
             let handler:express.RequestHandler= <express.RequestHandler> instance[route.function];
-            router.route(`${route.path}`)[route.method]([...middlewares.before, handler, ...middlewares.after, ...middlewares.error]) 
+            router.route(`${route.path}`)[route.method]([...middlewares_class.before,...middlewares.before, handler, ...middlewares.after, ...middlewares_class.after,...middlewares.error, ...middlewares_class.error]) 
             app.use(`${uses}${prefix}`, router);
-
         });
     })
     return app;
